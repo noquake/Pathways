54935d76b0af4225ef91f4688dc520701a21cd2c
kwaku.aa <kwakuagyapong478@gmail.com>
2025-11-22 21:01:28 -0500

    emedding to docker-dbs for test on GEEDORAH

--- chunk_and_embed.py
+++ chunk_and_embed.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer # type: ignore
 import psycopg2
+from pgvector.psycopg2 import register_vector
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
@@ -28,13 +29,13 @@ for md_path in Path("scratch").glob("*.md"):
     print(md_path.name, len(chunks))
 
 
-""" Testing to see if the output actually comes out as expected """
-output_path = Path("scratch/chunks.txt")
-with output_path.open("w") as f:
-    for i, chunk in enumerate(chunks):
-        f.write(f"\n\n===== CHUNK {i} =====\n\n")
-        f.write(chunk)
-        f.write("\n")
+# """ Testing to see if the output actually comes out as expected """
+# output_path = Path("scratch/chunks.txt")
+# with output_path.open("w") as f:
+#     for i, chunk in enumerate(chunks):
+#         f.write(f"\n\n===== CHUNK {i} =====\n\n")
+#         f.write(chunk)
+#         f.write("\n")
 
 def embed_chunks(chunks):
     embeddings = model.encode(chunks)
@@ -42,6 +43,35 @@ def embed_chunks(chunks):
         for i, emb in enumerate(embeddings):
             f.write(f"Chunk {i}:\n{emb.tolist() if hasattr(emb, 'tolist') else emb}\n\n")
     
-embed_chunks(chunks)
+embed_chunks(chunks) # Run the embedding function
 
-# conn = psycopg2.connect("dbname=pathways user=admin password=password host=your_host")
+# connect to the PostgreSQL database, casting the connection from variable -> to a vector type for psycopg2
+conn = psycopg2.connect("dbname=pathways user=admin password=password host=localhost port=5432")
+register_vector(conn)
+
+# creates a cursor object to start executing SQL commands
+cur = conn.cursor()
+cur.execute('CREATE EXTENSION IF NOT EXISTS vector')
+
+# create table of items with vector embeddings
+cur.execute('CREATE TABLE IF NOT EXISTS items (id bigserial PRIMARY KEY, text TEXT, embedding vector(384))')
+
+# embed by chunk, and insert into the database
+for chunk in chunks:
+    emb = model.encode(chunk)
+    cur.execute('INSERT INTO items (text, embedding) VALUES (%s, %s)', (chunk, emb))
+    conn.commit()
+
+""" Index types for pgvector """
+# Approximate Nearest Neighbor (ANN) indexes for faster search MEDIUM to LARGE datasets, bad space
+cur.execute('CREATE INDEX IF NOT EXISTS ON items USING hnsw (embedding vector_l2_ops)')
+# Cluster-based ANN 
+# cur.execute('CREATE INDEX ON items USING ivfflat (embedding vector_l2_ops) WITH (lists = 100)')
+
+# get the nearest neighbors for a given embedding/chunk to use for LLM context
+query = "What is Pathways?"
+query_emb = model.encode(query)
+cur.execute('SELECT text FROM items ORDER BY embedding <-> %s LIMIT 5', (query_emb,))
+results = cur.fetchall()
+for r in results:
+    print(r[0])
